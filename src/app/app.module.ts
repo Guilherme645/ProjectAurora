e3b66be83c67f791cade6c71029750768d3f7c9a
guilherme Carvalho da Silva <guilherme@viaapia.com.br>
2025-03-10 16:07:48 -0300

    importação routes feita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
 import { LoginComponent } from './components/login/login.component';
-import { RouterOutlet } from '@angular/router';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { CardComponent } from './components/card/card.component';
 import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 import { BuscarComponent } from './components/buscar/buscar.component';
 import { PageBuscaComponent } from './pages/page-busca/page-busca.component';
 import { CriarContaComponent } from './components/criar-conta/criar-conta.component';
 import { EsqueceuSenhaComponent } from './components/esqueceu-senha/esqueceu-senha.component';
-import { HeaderComponent } from './components/header/header.component';
 import { PageEsqueceuSenhaComponent } from './pages/page-esqueceu-senha/page-esqueceu-senha.component';
 import { PageCriarContaComponent } from './pages/page-criar-conta/page-criar-conta.component';
 import { HighSearchComponent } from './components/high-search/high-search.component';
@@ -34,38 +33,44 @@ import { SaveSearchComponent } from './components/save-search/save-search.compon
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { LocalizacaoComponent } from './components/localizacao/localizacao.component';
 
-@NgModule({ declarations: [
-        AppComponent,
-        LoginComponent,
-        PageLoginComponent,
-        CardComponent,
-        NavBarComponent,
-        SideBarComponent,
-        HeaderComponent,
-        BuscarComponent,
-        PageBuscaComponent,
-        CriarContaComponent,
-        EsqueceuSenhaComponent,
-        PageEsqueceuSenhaComponent,
-        PageCriarContaComponent,
-        HighSearchComponent,
-        InputBuscaComponent,
-        RelatorioModalComponent,
-        MensoesComponent,
-        ScrollTopComponent,
-        ModalAccountComponent,
-        TagFilterComponent,
-        VeiculosComponent,
-        ResultComponent,
-        FiltrosComponent,
-        SearcBasicComponent,
-        SaveSearchComponent,
-        CalendarComponent,
-        LocalizacaoComponent
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        RouterOutlet,
-        FormsModule,
-        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PageLoginComponent,
+    CardComponent,
+    NavBarComponent,
+    SideBarComponent,
+    HeaderComponent,
+    BuscarComponent,
+    PageBuscaComponent,
+    CriarContaComponent,
+    EsqueceuSenhaComponent,
+    PageEsqueceuSenhaComponent,
+    PageCriarContaComponent,
+    HighSearchComponent,
+    InputBuscaComponent,
+    RelatorioModalComponent,
+    MensoesComponent,
+    ScrollTopComponent,
+    ModalAccountComponent,
+    TagFilterComponent,
+    VeiculosComponent,
+    ResultComponent,
+    FiltrosComponent,
+    SearcBasicComponent,
+    SaveSearchComponent,
+    CalendarComponent,
+    LocalizacaoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule, // ✅ IMPORTANTE PARA AS ROTAS FUNCIONAREM
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+  bootstrap: [AppComponent] // ✅ GARANTA QUE ESTÁ CORRETO
+})
 export class AppModule { }
